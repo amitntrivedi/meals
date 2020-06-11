0225b81f619d27b014a6b6eaa90e03c9f8aa9e97
amitntrivedi <amitntrivedi@gmail.com>
2020-06-11 18:07:56 +0530

    Displayed Meals in MealsCategory

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,21 +1,27 @@
 import React from 'react'; 
-import {Text,View, StyleSheet,Button} from 'react-native'; 
-import {CATEGORIES} from '../data/dummy-data'; 
- 
+import {Text,View, StyleSheet,Button, FlatList} from 'react-native'; 
+import {CATEGORIES, MEALS} from '../data/dummy-data'; 
+
 
 const CategoryMealsScreen =(props ) =>{
     const catId= props.navigation.getParam('categoryId'); 
-    const selectedCategory = CATEGORIES.find(cat => cat.id === catId); 
+    const displayedMeals = MEALS.filter( meal => meal.categoryIds.indexOf(catId) >=0); 
 
+    const renderMealItem =(itemData) =>{
+        return(
+            <View >
+                <Text >Meals Title: {itemData.item.title}  </Text>
+            </View>
+        ); 
+    }; 
+    
     return(
         <View style={styles.screen}>
-            <Text >Category 123 Meals Screen {catId}</Text>
-            <Text >Category Title:  {selectedCategory.title}</Text>
-            <Text >Category Id:  {selectedCategory.id}</Text>
-            <Text >Category Color:  {selectedCategory.color}</Text>
-            <Button title='Go to Detail ' onPress={
-               ()=>{props.navigation.navigate({routeName: 'Detail'})}
-           } /> 
+            <FlatList
+                data={displayedMeals}
+                keyExtractor={(item,index) => item.id}
+                renderItem ={renderMealItem}
+            />
         </View>
     ); 
 
